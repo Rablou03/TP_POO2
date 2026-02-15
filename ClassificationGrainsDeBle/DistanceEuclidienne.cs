722269696a497b13c528cb2d48e04c9da83c793f
attafidele <atta.fidele@email.com>
2026-02-15 09:39:21 -0500

    Programme c#

---
+++ ClassificationGrainsDeBle/DistanceEuclidienne.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassificationGrainsDeBle
+{
+    internal class DistanceEuclidienne : Idistance
+    {
+        /* Lien 1 : https://stackoverflow.com/questions/34698649/calculate-the-euclidean-distance-between-an-array-in-c-sharp-with-function
+         * Lien 2 : https://learn.microsoft.com/fr-fr/archive/msdn-magazine/2019/may/test-run-weighted-k-nn-classification-using-csharp
+         */
+        public double Calculer(double[] a, double[] b)
+        {
+            double somme = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                somme += Math.Pow(a[i] - b[i], 2);
+            }
+            return Math.Sqrt(somme);
+        }
+    }
+}
---
+++ ClassificationGrainsDeBle/IClassifier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassificationGrainsDeBle
+{
+    internal interface IClassifier
+    {
+        void Entrainer(EnsenbleDonnees data);
+        string Predire(double[] caracteristiques);
+    }
+}
